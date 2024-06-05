152f7bc6e89d8308a1fd9f4af0b338161c41cc1f
coinblacker <coinblacker@gmail.com>
2024-06-05 23:03:12 +0900

    Thông điệp mô tả các thay đổi

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, World!"
+
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
